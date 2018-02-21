33b2f13348194dd40e562bb5ab70f1516d30f045
robdir <rhwickham@gmail.com>
2018-02-21 11:53:45 +0100

    add scss

---
+++ src/appContainer.js
@@ -0,0 +1,22 @@
+import React, {PureComponent} from 'react'
+
+
+export class appContainer extends PureComponent {
+
+
+// findCountries(url, request) {
+    
+// }
+
+render() {
+
+    return (
+        <div className="main_container">
+        <h4> This application renders data from Countries
+             REST API at your request! Search function coming soon... </h4> 
+        </div>
+        )
+    }
+}
+
+export default appContainer
