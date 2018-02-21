90d193008abc229e5ec572a7bf3c00b616b138c1
robdir <rhwickham@gmail.com>
2018-02-21 12:46:00 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Container from './appContainer';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Container/>
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div className="App">
+          <Container/>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/appContainer.js
+++ src/appContainer.js
@@ -1,5 +1,11 @@
 import React, {PureComponent} from 'react'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
 import './styles/css/appContainer.css'
+
+const inputStyle = {
+    color: 'black'
+}
 
 export class appContainer extends PureComponent {
 
@@ -8,6 +14,7 @@ export class appContainer extends PureComponent {
     
 // }
 
+
 render() {
 
     return (
@@ -15,6 +22,15 @@ render() {
         <h4> This application renders data from Countries
              REST API at your request!            {<br />}
              Search function coming soon... </h4> 
+            <div className="search_input">
+            <p> Enter country or region: </p>
+                <TextField
+                    id="search"
+                    ref="search"
+                    inputStyle={inputStyle}
+                />
+            </div>
+
         </div>
         )
     }
