214d7b5b5b1f92ef55c2cbb4b76bbc2bd093057f
robdir <rhwickham@gmail.com>
2018-03-02 14:34:38 +0100

    hello empty object

--- src/countrySingle.js
+++ src/countrySingle.js
@@ -9,13 +9,14 @@ class CountrySingle extends PureComponent {
         return (
 
             <div className="country_single">
+                    <div className="flaggins">
+                        <img src={this.props.flag}/>
+                    </div>
                 <p>Name: {this.props.name} </p>
                 <p>Demonym: {this.props.demonym} </p>
                 <p>Capital: {this.props.capital} </p>
                 <p>Population: {this.props.population} </p>
-                    <div className="flaggins">
-                        <img src={this.props.flag}/>
-                    </div>
+                {console.log(this.props)}
             </div>
         )
     }
