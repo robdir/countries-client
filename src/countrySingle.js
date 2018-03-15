000c2ce8eff84b35d876c15318ca822d11308069
robdir <rhwickham@gmail.com>
2018-03-15 16:22:56 +0100

    finished

--- src/countrySingle.js
+++ src/countrySingle.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 import Loading from './UI/Spinner'
 import './styles/css/countrySingle.css'
 
-    const numberWithCommas = (x) => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+const numberWithCommas = (x) => {
+return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 class CountrySingle extends PureComponent {
     constructor(props){
@@ -36,27 +36,27 @@ class CountrySingle extends PureComponent {
             subregion } = this.props
 
         return (
-                        <div className="country_single" id="single">
+                <div className="country_single" id="single">
 
-                            {this.state.isLoading ? (<Loading/> ) : (
-                                <div className="info">
-                                        <div className="flaggins">
-                                            <img src={flag} alt="flag" />
-                                        </div>
-                                    <p>Name: {name} </p>
-                                    <br/>
-                                    <p>Demonym: {demonym} </p>
-                                    <br/>
-                                    <p>Capital: {capital} </p>
-                                    <br />
-                                    <p>Population: {numberWithCommas(population)} </p>
-                                    <br />
-                                    <p>Continent: {region} </p>
-                                    <br />
-                                    <p>Region: {subregion} </p>
-                                </div> 
-                                )}
-                        </div>
+                    {this.state.isLoading ? (<Loading/> ) : (
+                        <div className="info">
+                                <div className="flaggins">
+                                    <img src={flag} alt="flag" />
+                                </div>
+                            <p>Name: {name} </p>
+                            <br/>
+                            <p>Demonym: {demonym} </p>
+                            <br/>
+                            <p>Capital: {capital} </p>
+                            <br />
+                            <p>Population: {numberWithCommas(population)} </p>
+                            <br />
+                            <p>Continent: {region} </p>
+                            <br />
+                            <p>Region: {subregion} </p>
+                        </div> 
+                        )}
+                </div>
 
         )
     }
